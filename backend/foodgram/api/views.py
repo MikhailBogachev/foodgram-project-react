a51b362e3db59aa5196fd461439d3fb7a1509738
Михаил Богачев <bogachevmikhailirkutsk@gmail.com>
2023-08-26 17:35:52 +0800

    debug ingredient filter and tags hex code 6

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -2,7 +2,7 @@ from django.db.models import Q
 from django.http.response import HttpResponse
 from django.contrib.auth import get_user_model
 from rest_framework.response import Response
-from rest_framework import viewsets, filters
+from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import DjangoModelPermissions, IsAuthenticated
 from djoser.views import UserViewSet as DjoserUserViewSet
