40515cb023b9e41d3ff1ebc65448a3b7b8d9a5dc
Михаил Богачев <bogachevmikhailirkutsk@gmail.com>
2023-08-26 17:01:50 +0800

    debug ingredient filter and tags hex code 2

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -2,6 +2,7 @@ from django_filters.rest_framework import FilterSet, filters
 
 from recipes.models import Ingredient
 
+
 class IngredientFilter(FilterSet):
     name = filters.CharFilter(lookup_expr='startswith')
 
